c93ac60d79ac0542dd31c6ccda3da55e13f12e42
wwjing1990 <38483288+wwjing1990@users.noreply.github.com>
2018-10-19 15:36:34 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,12 +10,14 @@ void myDisplay(void)
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
-  glutInit(&argc, argv);
+  	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);	//设置显示模式：RGBA颜色模式+单缓冲窗口
 	glutInitWindowPosition(100, 100);
 	glutInitWindowSize(400, 400);
-	glutCreateWindow("第一个OpenGL程序");			//创建OpenGL窗口
-	glutDisplayFunc(&myDisplay);					//显示回调
-	glutMainLoop();									//启动GLUT框架的运行
+	glutCreateWindow("第一个OpenGL程序");	    //创建OpenGL窗口
+	glutDisplayFunc(&myDisplay);		 	//显示回调
+	glutMainLoop();					//启动GLUT框架的运行
+	
+	
 	return a.exec();
 }
